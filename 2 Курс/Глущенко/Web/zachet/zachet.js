f65fd886237187cf6b92a04598e6a4f801f0528b
Glushchenko <glushchenko_nikita@bk.ru>
2021-12-22 12:56:56 +0300

    lastzakchetweb

--- 2 Курс/Глущенко/Web/zachet/zachet.js
+++ 2 Курс/Глущенко/Web/zachet/zachet.js
@@ -26,56 +26,61 @@ var vtb = new Map()
     }
     function summaproc(bank,vklad,years,save){
         var itog=0
-        if (vklad=="thous50"){
-                itog=50000
-            }
-            else if (vklad=="thous100"){
-                itog=100000
-            }
-            else if (vklad=="thous200"){
-                itog=20000
-            }
-        for (var i=0;i<years;i++){
-            if (vklad=="thous50"){
-                if (bank=="sber"){
-                    itog*=slovarznach.sber.thous50
-                }
-                else if (bank=="vtb"){
-                    itog*=slovarznach.vtb.thous50
-                }
-                else if (bank=="alfa"){
-                    itog*=slovarznach.alfa.thous50
-                }
-            }
-            else if (vklad=="thous100"){
-                if (bank=="sber"){
-                    itog*=slovarznach.sber.thous100
-                }
-                else if (bank=="vtb"){
-                    itog*=slovarznach.vtb.thous100
-                }
-                else if (bank=="alfa"){
-                    itog*=slovarznach.alfa.thous100
-                }
-            }
-            else if (vklad=="thous200"){
-                if (bank=="sber"){
-                    itog*=slovarznach.sber.thous200
-                }
-                else if (bank=="vtb"){
-                    itog*=slovarznach.vtb.thous200
-                }
-                else if (bank=="alfa"){
-                    itog*=slovarznach.alfa.thous200
-                }
-            }
-        }
-        if (save){
-        alert(itog.toFixed(2))
+        if (years==""){
+            alert("Введите количество лет")
         }
         else{
-            localStorage.setItem("summaProc",itog.toFixed(2));
-            alert("Сохранено в localStorage");
-            alert(localStorage.getItem("summaProc"))
+            if (vklad=="thous50"){
+                    itog=50000
+                }
+                else if (vklad=="thous100"){
+                    itog=100000
+                }
+                else if (vklad=="thous200"){
+                    itog=20000
+                }
+            for (var i=0;i<years;i++){
+                if (vklad=="thous50"){
+                    if (bank=="sber"){
+                        itog*=slovarznach.sber.thous50
+                    }
+                    else if (bank=="vtb"){
+                        itog*=slovarznach.vtb.thous50
+                    }
+                    else if (bank=="alfa"){
+                        itog*=slovarznach.alfa.thous50
+                    }
+                }
+                else if (vklad=="thous100"){
+                    if (bank=="sber"){
+                        itog*=slovarznach.sber.thous100
+                    }
+                    else if (bank=="vtb"){
+                        itog*=slovarznach.vtb.thous100
+                    }
+                    else if (bank=="alfa"){
+                        itog*=slovarznach.alfa.thous100
+                    }
+                }
+                else if (vklad=="thous200"){
+                    if (bank=="sber"){
+                        itog*=slovarznach.sber.thous200
+                    }
+                    else if (bank=="vtb"){
+                        itog*=slovarznach.vtb.thous200
+                    }
+                    else if (bank=="alfa"){
+                        itog*=slovarznach.alfa.thous200
+                    }
+                }
+            }
+            if (save){
+            alert(itog.toFixed(2))
+            }
+            else{
+                localStorage.setItem("summaProc",itog.toFixed(2));
+                alert("Сохранено в localStorage");
+                alert(localStorage.getItem("summaProc"))
+            }
         }
     }
