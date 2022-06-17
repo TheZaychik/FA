972d1a464d4431b568d063cada625d16d36caa95
Frich <iirubin2@gmail.com>
2022-06-17 23:38:49 +0300

    AndroidMagic
    Aga

---
+++ 2 Курс/Рубинов/Android/зач/Lab5_5/app/src/main/java/com/example/lab5_5/MessageReceiver.java
@@ -0,0 +1,21 @@
+package com.example.lab5_5;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class MessageReceiver extends BroadcastReceiver {
+
+    public MessageReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Обнаружено сообщение: " +
+                        intent.getStringExtra("com.example.broadcast.Message"),
+                Toast.LENGTH_LONG).show();
+
+    }
+
+}
---
+++ 2 Курс/Рубинов/Android/зач/Zach8/app/src/main/java/com/example/zach8/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.zach8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        intent = new Intent(this, MyService.class);
+
+    }
+
+    public void Notification1(View view){
+        startService(intent);
+    }
+}
