d303d4cc832ba3a3e64b0fad2482374a9501812c
Nikita Zaystev <nikitos945max@gmail.com>
2024-04-08 21:03:04 +0300

    tpp 2

---
+++ 4 Курс/Зайцев/ТПП/sem7/sem7_1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <omp.h>
+#include <locale>
+#include <ctime>
+using namespace std;
+int main()
+{
+    double start, end;
+    const int W = 10;
+    int arr[W][W];
+    start = omp_get_wtime();
+#pragma omp parallel
+    {
+       srand(time(0) + omp_get_thread_num());
+#pragma omp for schedule(dynamic, 6)
+        for (int i = 0; i < W; ++i)
+            for (int j = 0; j < W; ++j)
+                arr[i][j] = rand() % 50;
+    }
+    end = omp_get_wtime();
+    for (int i = 0; i < W; i++)
+    {
+        for (int j = 0; j < W; j++)
+        {
+            cout << arr[i][j];
+            cout << ' ';
+        }
+        cout << "\n";
+    }
+    printf("Compute Time: %f seconds\n", end - start);
+}
