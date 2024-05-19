75d77337e6ba31c9b51f9c509792569bce7c0532
Nikita Zaystev <nikitos945max@gmail.com>
2024-05-19 21:53:08 +0300

    #

--- 4_Курс/Зайцев/ТПП/sem13/MPIMess/MPIMess1.py
+++ 4_Курс/Зайцев/ТПП/sem13/MPIMess/MPIMess1.py
@@ -1,6 +1,6 @@
 import sys
 from mpi4py import MPI
-from numpy import empty , array , int32 , float64 , dot
+from numpy import empty, array, int32, float64, dot
 
 # определяем коммуникатор comm – группу процессов, которые участвуют в вычислениях
 comm = MPI.COMM_WORLD
@@ -11,21 +11,21 @@ rank = comm.Get_rank()
 numprocs = comm.Get_size()
 
 if rank == 0:
-  f1 = open('in.dat ', 'r')
-  N = array(int32(f1.readline ()))
-  M = array(int32(f1.readline ()))
-  f1.close ()
+    f1 = open("/home/user/proj/sem13/MPIMess/in.dat", "r")
+    N = array(int32(f1.readline()))
+    M = array(int32(f1.readline()))
+    f1.close()
 else:
-  N = array (0, dtype=int32)
-  M = array (0, dtype=int32)
+    N = array(0, dtype=int32)
+    M = array(0, dtype=int32)
 
 if rank == 0:
-  comm.send(M, dest =1, tag =0)
-  print (f'Variable N on process {rank} is: {N}')
-  print (f'Variable M on process {rank} is: {M}')
+    comm.send(M, dest=1, tag=0)
+    print(f"Variable N on process {rank} is: {N}")
+    print(f"Variable M on process {rank} is: {M}")
 elif rank == 1:
-  N = comm.recv(source =0, tag =0)
-  print (f'Variable N on process {rank} is: {N}')
-  print (f'Variable M on process {rank} is: {M}')
+    N = comm.recv(source=0, tag=0)
+    print(f"Variable N on process {rank} is: {N}")
+    print(f"Variable M on process {rank} is: {M}")
 else:
-  sys.exit(0)
+    sys.exit(0)
--- 4_Курс/Зайцев/ТПП/sem13/MPIMess/MPIdeadlock.py
+++ 4_Курс/Зайцев/ТПП/sem13/MPIMess/MPIdeadlock.py
@@ -6,23 +6,23 @@ comm = MPI.COMM_WORLD
 rank = comm.rank
 print("my rank is %i " % (rank))
 
-# Для процесса rank, равного 1, отправляет и получает данные от процесса со значением 
+# Для процесса rank, равного 1, отправляет и получает данные от процесса со значением
 # rank равным 5
-if rank==1: 
-    data_send = "a" 
+if rank == 1:
+    data_send = "a"
     destination_process = 5
     sourse_process = 5
     comm.send(data_send, dest=destination_process)
-    data_received=comm.recv(source=sourse_process)
-    print("sending data %s " %data_send + "to process %d" %destination_process)
-    print("data received is = %s" %data_received + "from process %d" %sourse_process)
+    data_received = comm.recv(source=sourse_process)
+    print("sending data %s " % data_send + "to process %d" % destination_process)
+    print("data received is = %s" % data_received + "from process %d" % sourse_process)
 
-# Процесс 5 
-if rank==5: 
+# Процесс 5
+if rank == 5:
     data_send = "b"
     destination_process = 1
     sourse_process = 1
-    comm.send(data_send, dest=destination_process) 
-    data_received=comm.recv(source=sourse_process)
-    print ("sending data %s " %data_send + "to process %d" %destination_process)
-    print ("data received is = %s" %data_received + "from process %d" %sourse_process)
+    comm.send(data_send, dest=destination_process)
+    data_received = comm.recv(source=sourse_process)
+    print("sending data %s " % data_send + "to process %d" % destination_process)
+    print("data received is = %s" % data_received + "from process %d" % sourse_process)
